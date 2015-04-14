21151053d1566bb3ca7b8d4ddfd3c0d6e6c263ea
Richi Priego <arturopriegothe3@gmail.com>
2015-04-14 16:12:33 -0600

    Added slick
    Added slick to lesson show page allowing users to scroll through
    lessons.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,67 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.slick
 //= require foundation
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ 
+
+  $(document).foundation();
+
+
+// $('.responsive').slick({
+//         // dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         responsive: [{
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 6,
+//                 slidesToScroll: 1,
+//                 // centerMode: true,
+
+//             }
+
+//         }, {
+//             breakpoint: 800,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 2,
+//                 dots: true,
+//                 infinite: true,
+
+//             }
+
+
+//         }, {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//                 dots: true,
+//                 infinite: true,
+                
+//             }
+//         }, {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 dots: true,
+//                 infinite: true,
+//                 autoplay: true,
+//                 autoplaySpeed: 2000,
+//             }
+//         }]
+//     });
+
+
+
+
+
+
+ });
