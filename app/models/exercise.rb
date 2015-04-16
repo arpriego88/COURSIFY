dee73edcd8f21e4eac3e45a127ae2a76e57ee2d8
Richi Priego <arturopriegothe3@gmail.com>
2015-04-16 17:48:14 -0600

    changed the exercise model
    Changed the exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,4 @@
 class Exercise < ActiveRecord::Base
   belongs_to :lesson
-  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :courses
+  has_many :courses, dependant: :destroy
+  has_many :exercises, dependant: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
