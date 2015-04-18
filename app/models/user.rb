24e56bd736b7e338331102366d2bb6b461ce9575
Chris Collinsworth <cmcollin41@gmail.com>
2015-04-18 16:03:36 -0600

    fixed migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ActiveRecord::Base
   has_many :subscribes, dependent: :destroy
   has_many :courses, through: :subscribes
-<<<<<<< HEAD
-
-  enum role: %w(student teacher admin)
-=======
   has_many :completes
   has_many :lessons, through: :completes
->>>>>>> master
+
+  enum role: %w(student teacher admin)
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
