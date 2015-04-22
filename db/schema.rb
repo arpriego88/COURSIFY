e06141e5550deaec28cb6c0c3ee21f93579936e3
rayrowe125 <gonzai125@gmail.com>
2015-04-22 12:27:46 -0600

    mailer is finished but the data base is effed

--- db/schema.rb
+++ db/schema.rb
@@ -11,51 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417201748) do
+ActiveRecord::Schema.define(version: 20150421033249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "completes", force: :cascade do |t|
+  create_table "purchases", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "lesson_id"
+    t.integer  "shirt_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "completes", ["lesson_id"], name: "index_completes_on_lesson_id", using: :btree
-  add_index "completes", ["user_id"], name: "index_completes_on_user_id", using: :btree
-
-  create_table "courses", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "complete",    default: false
-  end
-
-  create_table "exercises", force: :cascade do |t|
-    t.text     "response"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "lessons", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "video_url"
-    t.string   "video_time"
-    t.text     "content"
-    t.string   "exercise_name"
-    t.text     "exercise_content"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "course_id"
-    t.boolean  "complete",         default: false, null: false
-    t.string   "wistia_video"
-  end
-
-  add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
+  add_index "purchases", ["shirt_id"], name: "index_purchases_on_shirt_id", using: :btree
+  add_index "purchases", ["user_id"], name: "index_purchases_on_user_id", using: :btree
 
   create_table "shirts", force: :cascade do |t|
     t.string   "name"
@@ -71,16 +40,6 @@ ActiveRecord::Schema.define(version: 20150417201748) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
-
-  create_table "subscribes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "subscribes", ["course_id"], name: "index_subscribes_on_course_id", using: :btree
-  add_index "subscribes", ["user_id"], name: "index_subscribes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -107,9 +66,6 @@ ActiveRecord::Schema.define(version: 20150417201748) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "completes", "lessons"
-  add_foreign_key "completes", "users"
-  add_foreign_key "lessons", "courses"
-  add_foreign_key "subscribes", "courses"
-  add_foreign_key "subscribes", "users"
+  add_foreign_key "purchases", "shirts"
+  add_foreign_key "purchases", "users"
 end
