6986fe4349429cf1679ccf60b9ccf593365b52d8
Richi Priego <arturopriegothe3@gmail.com>
2015-04-22 15:35:42 -0600

    added devise omniauthable to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:twitter]
 end
