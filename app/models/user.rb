f2a5213350f789904392271a774aaac9a5ea2a9b
Richi Priego <arturopriegothe3@gmail.com>
2015-04-23 10:27:45 -0600

    OmniAuth Twitter
    Made it to where users can sign in through OmniAuth Twitter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,23 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, :omniauth_providers => [:twitter]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = "changeme@#{auth.uid}#{auth.provider}.com"
+      user.name = auth.info.name
+      user.password = user.password_confirmation = Devise.friendly_token[0,20]
+    end 
+  end
+
+  # def self.new_with_sessions(params, session)
+  #   super.tap do |user|
+  #     if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
+  #       user.provider = session["devise.twitter_data"]["provider"]
+  #       user.uid = session["devise.twitter_data"]["uid"]
+  #       user.name = data["name"] if user.name.blank?
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 end
