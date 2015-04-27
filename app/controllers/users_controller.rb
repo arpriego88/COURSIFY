3804e3aa453ecb80784d9d3d027627f8b07c4293
rayrowe125 <gonzai125@gmail.com>
2015-04-27 11:40:37 -0600

    Wistia Data Basics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,20 @@ class UsersController < ApplicationController
 
   def profile
     @user = current_user
+    @video = []
+    @user.courses.each do |course|
+      course.lessons.each do |lesson|
+        @wistia = lesson.wistia_video
+        @video << Wistia::Stats::Media.get(@wistia)
+      end
+    end
+    @video_map = []
+    @user.courses.each do |course|
+      course.lessons.each do |lesson|
+        @wistia = lesson.wistia_video
+        @video_map << Wistia::Stats::Events.get(@wistia)
+      end
+    end
   end
+
 end
