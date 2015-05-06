01a03c0bfd4014f194804b7da40f4665ba976650
Chris Collinsworth <cmcollin41@gmail.com>
2015-05-06 11:19:35 -0600

    fixed grid; fixed ajax

--- app/controllers/completes_controller.rb
+++ app/controllers/completes_controller.rb
@@ -4,8 +4,10 @@ class CompletesController < ApplicationController
   def create
     @lesson = Lesson.find(params[:lesson_id])
     @complete = Complete.create(user_id: current_user.id, lesson_id: @lesson.id)
-    if params['from_lesson'] == 'true'
+    if params['from_lesson'] == 'true' && @lesson.next_lesson.present?
       redirect_to course_lesson_path(@lesson.course, @lesson.next_lesson)
+    elsif @next_lesson.present? == false
+      redirect_to course_path(@lesson.course)
     else
       redirect_to request.referer
     end
